a2fd22ccc834fffd35f28f7aedafe7652c0a7eb1
shunterer <shaun7777@yahoo.com>
2014-09-22 00:11:21 +1000

    update

--- run_analysis.r
+++ run_analysis.r
@@ -1,35 +1,30 @@
-
-# variable that specifies the path to the data
+#PATH
 yourPath = "./UCIHARDataset/"
 
-# load x and y for Test and Train
+#load in training and testing data
 xTrain = read.table(paste(yourPath, "train/X_train.txt", sep=""))
 yTrain = read.table(paste(yourPath, "train/y_train.txt", sep=""))
 xTest = read.table(paste(yourPath, "test/X_test.txt", sep=""))
 yTest = read.table(paste(yourPath, "test/y_test.txt", sep=""))
-
 
 # load variable names
 varNames = read.table(paste(yourPath, "features.txt", sep="")); varNames = varNames[,2];
 activityLabels = read.table(paste(yourPath, "activity_labels.txt", sep="")); 
 
 
-# correctly name the columns of the data
+# assign column names of the data
 names(xTrain) <- varNames
 names(xTest) <- varNames
 
 ## Merge Test and Train
-xMatrix = rbind(xTest,xTrain) # from 1 to 7532 they were used for training, from 7533 to 10299 they were used for testing
-yVector = rbind(yTest,yTrain) # from 1 to 7532 they were used for training, from 7533 to 10299 they were used the result of testing
+xMatrix = rbind(xTest,xTrain) 
+yVector = rbind(yTest,yTrain)
 
 # which variables correspond to means 
 whichMeans = grep("[m]ean", varNames)
 
-# correspondingly update xMatrix
+# update xMatrix
 xMatrixReduced = xMatrix[,whichMeans]
-
-# write it:
-write.csv(xMatrixReduced,file=paste(yourPath, "xMatrixReduced.csv", sep=""))
 
 yVectorLabels=vector(mode="character", length=length(yVector))
 
@@ -55,14 +50,14 @@ subjectNamesVector = rbind(subjectNamesTrain, subjectNamesTest)
 
 
 ### creating a second dataset that has the means of each variable for each subject
-newData = matrix(ncol=length(names(xMatrixReduced)), nrow=length(unique(subjectNamesVector)))
-rownames(newData) = unique(subjectNamesVector); 
-colnames(newData) = names(xMatrixReduced);
+tidyData = matrix(ncol=length(names(xMatrixReduced)), nrow=length(unique(subjectNamesVector)))
+rownames(tidyData) = unique(subjectNamesVector); 
+colnames(tidyData) = names(xMatrixReduced);
 
 # for each subject calculate the mean of all variables
 for(s in unique(subjectNamesVector)){
   w = which(subjectNamesVector == s)
   cm = colMeans(xMatrixReduced[w,])
-  newData[s,] = cm
+  tidyData[s,] = cm
 }
-write.csv(newData,file=paste(yourPath, "newData.csv", sep=""))
+write.csv(tidyData,file=paste(yourPath, "tidyData.csv", sep=""))
